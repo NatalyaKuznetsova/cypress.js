7c8bcbee6b12b289a2f97e956791eaa0bf9754a5
Natalia Kuznetsova <102148463+NatalyaKuznetsova@users.noreply.github.com>
2022-10-18 11:08:05 +0300

    Update wrongpassword.js

--- cypress/integration/Viskonsin/wrongpassword.js
+++ cypress/integration/Viskonsin/wrongpassword.js
@@ -2,8 +2,8 @@ describe('Тестирование qa.studio', function () {
 
     it('Проверка на валидную авторизацию', function () {
         cy.visit('https://login.qa.studio/');
-        cy.get("#mail").type('german@dolnikov.ru');
-        cy.get("#pass").type('germaNdol5');
+        cy.get("#mail").type('Валидная почта');
+        cy.get("#pass").type('Невалидный пароль');
         cy.get("#loginButton").click();
 
         cy.contains('Такого логина или пароля нет');
