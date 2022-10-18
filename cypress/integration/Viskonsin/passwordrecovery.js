6a64fa2d545afd6cf1ec3b7f1eb080e50be53349
Natalia Kuznetsova <102148463+NatalyaKuznetsova@users.noreply.github.com>
2022-10-18 11:08:53 +0300

    Update passwordrecovery.js

--- cypress/integration/Viskonsin/passwordrecovery.js
+++ cypress/integration/Viskonsin/passwordrecovery.js
@@ -3,7 +3,7 @@ describe('Тестирование qa.studio', function () {
     it('Проверка логики восстановления пароля', function () {
         cy.visit('https://login.qa.studio/');
         cy.contains('Забыли пароль').click();
-        cy.get('#mailForgot').type('german@dolnikov.ru');
+        cy.get('#mailForgot').type('Валидная почта');
         cy.get('#restoreEmailButton').click();
         
         cy.contains('Успешно отправили пароль на e-mail');
