3c3fa8134578bf6b51a1928404d1fa285e899f27
Natalia Kuznetsova <102148463+NatalyaKuznetsova@users.noreply.github.com>
2022-10-18 11:09:20 +0300

    Update positiventer.js

--- cypress/integration/Viskonsin/positiventer.js
+++ cypress/integration/Viskonsin/positiventer.js
@@ -2,8 +2,8 @@ describe('Тестирование qa.studio', function () {
 
     it('Проверка на валидную авторизацию', function () {
         cy.visit('https://login.qa.studio/');
-        cy.get('#mail').type('german@dolnikov.ru');
-        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#mail').type('Валидная почта');
+        cy.get('#pass').type('Валидный пароль');
         cy.get('#loginButton').click();
 
         cy.contains('Авторизация прошла успешно');
